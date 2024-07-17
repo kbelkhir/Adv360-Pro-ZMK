c1f6d5a9b8c367477263d397b561c189499f5eec
Abdelkarim Belkhir <abdelk.belkhir@gmail.com>
2024-07-17 09:40:22 -0400

    Create cust_keys.h

---
+++ config/cust_keys.h
@@ -0,0 +1,6 @@
+// Left Hyper Key: Left Shift ⇧ + Left Alt + Left Control + Left GUI (Windows / Command / Meta)
+
+#define LHYPER LC(LA(LS(LEFT_GUI)))
+
+// Left Meh Key: Left Shift ⇧ + Left Alt + Left Control
+#define LMEH LC(LA((LEFT_GUI))
